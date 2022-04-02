ad38d1b8ce8ce613c129a074d72a4169a9eb6070
herenajoshika <101052348+herenajoshika@users.noreply.github.com>
2022-04-02 23:26:07 +0530

    Add files via upload
    done

---
+++ 3_Implementation/src/options.c
@@ -0,0 +1,25 @@
+/**
+ * @file options.c
+ * @author Herena Joshika A S
+ * @brief Displaying various options in the rental service
+ * @version 0.1
+ * @date 2022-03-27
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <stdio.h>
+#include "carrent.h"
+
+int options() {
+
+    printf("******************Hello!*******************\n");
+    printf("**********Welcome to SPARKLES CAR RENTAL SERVICE***********\n\n");
+    printf("****Please choose one of the options below****\n\n");
+    printf("< 1 >  Rent a car\n");
+    printf("< 2 >  Car Wash\n");
+    printf("< 3 >  Exit\n");
+    return 6;
+
+}
